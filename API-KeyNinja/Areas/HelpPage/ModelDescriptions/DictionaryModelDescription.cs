7fa55050987ed98100fe08dcf8b829d229c92e42
Richardo Alvin <richardo.alvin@yahoo.com>
2019-08-07 16:59:51 +1000

    Add project files.

---
+++ API-KeyNinja/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_KeyNinja.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ API-KeyNinja/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_KeyNinja.WebApiApplication" Language="C#" %>
